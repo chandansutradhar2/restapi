afc3d503bbecee92a21735b6b165d716c0c5057b
chandan <chandansutradhar2@gmail.com>
2022-11-15 12:19:13 +0530

    added login

--- index.js
+++ index.js
@@ -11,12 +11,35 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.post("/login", (req, res) => {
+  console.log(req.body);
+  if (req.body.email.includes("chandan")) {
+    res.status(200).send({
+      status: true,
+      data: {
+        dob: "06/03/1983",
+        fullName: "Chandan Naresh",
+        email: "chandan@gmail.com",
+        mobileNo: "8080811145",
+      },
+      msg: "login success",
+    });
+  } else {
+    res.status(200).send({
+      status: false,
+      data: null,
+      msg: "invalid credentials",
+    });
+  }
+});
+
 app.post("/register", (req, res) => {
   console.log(req.body);
   //todo: connect to db and save user data
-  res.status(200).json({
-    status: true,
-    msg: "user created successfully",
+
+  res.status(501).json({
+    status: false,
+    msg: "failed to create account. pls contact system admin",
   });
 });
 
